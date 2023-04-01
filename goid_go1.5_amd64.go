d9644702e66ced9a2d215bc3375c57bc0cd05813
lixiantu <lixiantu@bytedance.com>
2023-04-01 17:43:21 +0800

    test

--- goid_go1.5_amd64.go
+++ goid_go1.5_amd64.go
@@ -20,6 +20,136 @@
 
 package goid
 
+import (
+	"runtime"
+	"strconv"
+	"sync/atomic"
+	"unsafe"
+	_ "unsafe"
+)
+
 func Get() int64
 
 func GetDefer() uintptr
+
+func GetPC() uintptr
+
+func GetPCBP() (uintptr, uintptr)
+
+func Ret(p interface{})
+
+//go:linkname deferreturn runtime.deferreturn
+func deferreturn(s string)
+
+//go:linkname printnl runtime.printnl
+func printnl()
+
+//go:linkname printstring runtime.printstring
+func printstring(s string)
+
+var helloWorld = "你好，世界"
+
+func Print()
+
+//go:linkname callers runtime.callers
+func callers(skip int, pcbuf []uintptr) int
+
+func Getcallerpc() uintptr
+
+func GetG() uintptr
+
+type Log struct {
+	PC   uintptr
+	Code int64
+	Msg  string
+}
+
+func NewLog(code int, msg string) Log
+
+var (
+	mCache3 unsafe.Pointer = func() unsafe.Pointer {
+		m := make(map[uintptr]string)
+		// for i := 0; i < 1000; i++ {
+		// 	m[uintptr(i)] = fmt.Sprintf("%d", i)
+		// }
+		return unsafe.Pointer(&m)
+	}()
+)
+
+func (l Log) LineNO() (line string) {
+	mPCs := *(*map[uintptr]string)(atomic.LoadPointer(&mCache3))
+	line, ok := mPCs[l.PC]
+	if !ok {
+		file, n := runtime.FuncForPC(l.PC).FileLine(l.PC)
+		line = file + ":" + strconv.Itoa(n)
+		mPCs2 := make(map[uintptr]string, len(mPCs)+10)
+		mPCs2[l.PC] = line
+		for {
+			p := atomic.LoadPointer(&mCache3)
+			mPCs = *(*map[uintptr]string)(p)
+			for k, v := range mPCs {
+				mPCs2[k] = v
+			}
+			swapped := atomic.CompareAndSwapPointer(&mCache3, p, unsafe.Pointer(&mPCs2))
+			if swapped {
+				break
+			}
+		}
+	}
+	return
+}
+
+type Line uintptr
+
+func NewLine() Line
+
+var (
+	mCache4 unsafe.Pointer = func() unsafe.Pointer {
+		m := make(map[Line]string)
+		return unsafe.Pointer(&m)
+	}()
+)
+
+func (l Line) LineNO() (line string) {
+	mPCs := *(*map[Line]string)(atomic.LoadPointer(&mCache3))
+	line, ok := mPCs[l]
+	if !ok {
+		file, n := runtime.FuncForPC(uintptr(l)).FileLine(uintptr(l))
+		line = file + ":" + strconv.Itoa(n)
+		mPCs2 := make(map[Line]string, len(mPCs)+10)
+		mPCs2[l] = line
+		for {
+			p := atomic.LoadPointer(&mCache3)
+			mPCs = *(*map[Line]string)(p)
+			for k, v := range mPCs {
+				mPCs2[k] = v
+			}
+			swapped := atomic.CompareAndSwapPointer(&mCache3, p, unsafe.Pointer(&mPCs2))
+			if swapped {
+				break
+			}
+		}
+	}
+	return
+}
+
+// /usr/local/go/src/runtime/traceback.go:187
+// frame.fp = frame.sp + uintptr(funcspdelta(f, frame.pc, &cache))
+// 表明: funcspdelta 可以用于查找stack中sp和fb间的距离
+// 同时要注意,很多内联的函数,的特殊操作 ,,, ,,,
+
+// pc++
+// /usr/local/go/src/runtime/traceback.go
+/*
+// Normally, pc is a return address. In that case, we want to look up
+// file/line information using pc-1, because that is the pc of the
+// call instruction (more precisely, the last byte of the call instruction).
+// Callers expect the pc buffer to contain return addresses and do the
+// same -1 themselves, so we keep pc unchanged.
+// When the pc is from a signal (e.g. profiler or segv) then we want
+// to look up file/line information using pc, and we store pc+1 in the
+// pc buffer so callers can unconditionally subtract 1 before looking up.
+// See issue 34123.
+// The pc can be at function entry when the frame is initialized without
+// actually running code, like runtime.mstart.
+*/
--- goid_go1.5_amd64.s
+++ goid_go1.5_amd64.s
@@ -15,7 +15,7 @@
 
 // Assembly to mimic runtime.getg.
 
-//go:build (amd64 || amd64p32) && gc && go1.5
+// go:build (amd64 || amd64p32) && gc && go1.5
 // +build amd64 amd64p32
 // +build gc
 // +build go1.5
@@ -35,4 +35,96 @@ TEXT ·GetDefer(SB),NOSPLIT,$0-8
 	MOVQ (TLS), R14
 	MOVQ g__defer(R14), R13
 	MOVQ R13, ret+0(FP)
+	RET
+
+// func GetPC() uintptr
+TEXT ·GetPC(SB),NOSPLIT,$0-8
+	MOVQ retpc-8(FP), AX // MOVQ (SP), R13
+	MOVQ AX, ret+0(FP)
+	RET
+
+// func GetPCBP() (uintptr,uintptr)
+TEXT ·GetPCBP(SB),NOSPLIT,$0-16
+	MOVQ retpc-8(FP), AX 
+	MOVQ AX, ret+0(FP)
+	MOVQ retbp-16(FP), AX 
+	MOVQ AX, ret2+8(FP)
+	RET
+
+
+// func Retx()
+TEXT ·Retx(SB), NOSPLIT, $0-8
+	MOVQ	+8(BP), BX
+	MOVQ	BP, AX
+	ADDQ	$16,AX
+	MOVQ	+0(BP), BP
+	MOVQ	AX,SP
+	JMP	BX
+
+
+
+// func Ret1(p uintptr)
+TEXT ·Ret1(SB), NOSPLIT, $0-16
+	CMPQ	p+0(FP), $0
+	JHI	unwind
+	RET
+unwind:
+	MOVQ	BP, SP
+	ADDQ	$16,SP
+	MOVQ	+0(BP), BP
+	CALL    runtime·deferreturn(SB)
+	JMP	-8(SP)
+
+
+
+// func Ret1(p uintptr)
+TEXT ·Ret(SB), NOSPLIT, $0-16
+	CMPQ	p+0(FP), $0
+	JHI	unwind
+	RET
+unwind:
+	MOVQ	BP, SP
+	// ADDQ	$16,SP
+	ADDQ	$16,SP
+	MOVQ	+0(BP), BP
+	//CALL runtime·deferreturn(SB)
+	JMP	-8(SP)
+
+
+TEXT ·Print(SB), $16-0
+	MOVQ ·helloWorld+0(SB), AX
+	MOVQ AX, 0(SP)
+	MOVQ ·helloWorld+8(SB), BX
+	MOVQ BX, 8(SP)
+	CALL runtime·printstring(SB)
+	CALL runtime·printnl(SB)
+	RET
+
+
+TEXT ·Getcallerpc(SB), $0-8
+	CALL runtime·callers(SB)
+	RET
+
+// func GetG(p uintptr)
+// TEXT ·GetG(SB), NOSPLIT|ABIInternal, $0-8
+// 	CALL runtime·getg(SB)
+// 	RET
+
+//NewLog(code int, msg string) Log
+TEXT    ·NewLog(SB), NOSPLIT, $0-56
+    MOVQ 	retpc-8(FP), AX
+	MOVQ 	AX, ret+24(FP)
+	MOVQ 	code+0(FP), AX
+	MOVQ 	AX, ret+32(FP)
+	MOVQ 	msg+8(FP), AX
+	MOVQ 	AX, ret+40(FP)
+	MOVQ 	msg+16(FP), AX
+	MOVQ 	AX, ret+48(FP)
+	RET
+
+
+//NewLine() Line
+TEXT    ·NewLine(SB), NOSPLIT, $0-8
+    MOVQ 	retpc-8(FP), AX
+	MOVQ 	AX, ret+0(FP)
 	RET
